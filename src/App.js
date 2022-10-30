dc5e7cd77a49721649facfb29f7fed72d8a52a2d
Karol <freespirits1998@gmail.com>
2022-10-30 16:21:32 -0400

    task 3 pt 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CampsitesList from "./features/campsites/CampsitesList.js";
 import Header from './components/Header';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
