e35d6374913405b8fe4142952f46ed9724b821b3
Karol <freespirits1998@gmail.com>
2022-11-17 20:33:41 -0500

    Redux for ALL

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,10 +1,11 @@
+import { useSelector } from "react-redux";
 import { Col } from "reactstrap";
 import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
 
 const CommentsList = ({ campsiteId }) => {
-  const comments = selectCommentsByCampsiteId(campsiteId);
+  const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
   if (comments && comments.length > 0) {
     return (
       <Col md="5" className="m-1">
--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,9 +1,10 @@
+import { useSelector } from 'react-redux';
 import { Col } from "reactstrap";
 import Partner from "./Partner";
 import { selectAllPartners } from "./partnersSlice";
 
 const PartnersList = () => {
-  const partners = selectAllPartners();
+  const partners = useSelector(selectAllPartners);
 
   return (
     <Col className="mt-4">
