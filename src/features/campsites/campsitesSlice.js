abc2b3212faf7435eb8f2f748b5cce0533053ee1
Karol <freespirits1998@gmail.com>
2022-11-17 19:36:47 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,11 +1,20 @@
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+};
+
+const campsitesSlice = createSlice({ name: "campsites", initialState });
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
