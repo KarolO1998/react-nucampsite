f7d2ec3255ed22a43e9e5f0391fc2f6a2c1ab3f4
Karol <freespirits1998@gmail.com>
2022-11-17 20:41:04 -0500

    add commit and middleware

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import {
   Button,
   Modal,
@@ -9,9 +10,12 @@ import {
 } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { addComment } from "./commentsSlice";
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = (values) => {
     const comment = {
@@ -19,8 +23,10 @@ const CommentForm = ({ campsiteId }) => {
       rating: values.rating,
       author: values.author,
       text: values.commentText,
+      date: new Date(Date.now()).toISOString(),
     };
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
 
